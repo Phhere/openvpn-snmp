ba5ee7cbe8ceaa6878ccfb924ab381af4494fd22
Rehs, Philipp Helo <philipp.rehs@uni-duesseldorf.de>
2015-05-19 09:56:44 +0200

    Implemented snmp tables and config file

--- openvpn-agent.py
+++ openvpn-agent.py
@@ -41,6 +41,8 @@
 import sys, os, signal
 import optparse
 import pprint
+import json
+import re
 
 # Make sure we use the local copy, not a system-wide one
 sys.path.insert(0, os.path.dirname(os.getcwd()))
@@ -63,6 +65,13 @@ parser.add_option(
 	dest="persistencedir",
 	help="Sets the path to the persistence directory",
 	default="/var/lib/net-snmp"
+)
+parser.add_option(
+	"-c",
+	"--configfile",
+	dest="configfile",
+	help="path of the json configuration file",
+	default="openvpn.json"
 )
 (options, args) = parser.parse_args()
 
@@ -136,10 +145,12 @@ simpleDisplayString = agent.DisplayString(
 	initval = "Nice to meet you"
 )'''
 
+'''
 serverTableLength = agent.Unsigned32(
 	oidstr   = "OPENVPN-MIB::openvpnServerTableLength",
 	writable = False
 )
+'''
 
 serverTable = agent.Table(
 	oidstr = "OPENVPN-MIB::openvpnServerTable",
@@ -147,8 +158,8 @@ serverTable = agent.Table(
 		agent.DisplayString()
 	],
 	columns = [
-		(2, agent.Integer32(0),
-		(3, agent.Unsigned32(0),
+		(2, agent.Integer32(0)),
+		(3, agent.Unsigned32(0)),
 		(4, agent.Unsigned32(0))
 	],
 	counterobj = agent.Unsigned32(
@@ -156,10 +167,12 @@ serverTable = agent.Table(
 	)
 )
 
+'''
 userTableLength = agent.Unsigned32(
 	oidstr   = "OPENVPN-MIB::openvpnUserTableLength",
 	writable = False
 )
+'''
 
 userTable = agent.Table(
 	oidstr = "OPENVPN-MIB::openvpnUserTable",
@@ -167,8 +180,8 @@ userTable = agent.Table(
 		agent.DisplayString()
 	],
 	columns = [
-		(2, agent.DisplayString(),
-		(3, agent.Unsigned32(0),
+		(2, agent.DisplayString()),
+		(3, agent.Unsigned32(0)),
 		(4, agent.Unsigned32(0))
 	],
 	counterobj = agent.Unsigned32(
@@ -248,25 +261,67 @@ def HupHandler(signum, frame):
 	DumpRegistered()
 signal.signal(signal.SIGHUP, HupHandler)
 
+def logFileParser(lines):
+	regex = r'^([\w\.[a-z]+),([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+),([0-9]+),([0-9]+),(.*)'
+	userlist = []
+	server = {'send': 0, 'recv': 0};
+	for line in lines:
+		m = re.match(regex, line)
+		if m is not None:
+			t_send = m.group(4)
+			t_recv = m.group(3)
+			user = {'name': m.group(1),'ip': m.group(2), 'recv': t_recv, 'send': t_send, 'date': m.group(5)}
+			userlist.append(user)
+			server['send'] = server['send'] + int(t_send)
+			server['recv'] = server['recv'] + int(t_recv)
+	
+	server['users'] = userlist;
+	return server;
+			
+			
+
 # The simple agent's main loop. We loop endlessly until our signal
 # handler above changes the "loop" variable.
 print "{0}: Serving SNMP requests, send SIGHUP to dump SNMP object state, press ^C to terminate...".format(prgname)
+
+with open(options.configfile) as data_file:    
+    serverList = json.load(data_file)
 
 loop = True
 while (loop):
 	# Block and process SNMP requests, if available
 	agent.check_and_process()
+	
+	serverTable.clear();
+	userTable.clear();
+	for i in range(0, len(serverList['servers'])):
+		s = serverList['servers'][i]
+		if os.access(s['logFile'],os.R_OK):
+			fh = open(s['logFile'],"r")
+			fileContent = fh.readlines();
+			serverData = logFileParser(fileContent);
+			tmpRow = serverTable.addRow([agent.DisplayString(s['name'])])
+			tmpRow.setRowCell(2, agent.Integer32(len(serverData['users'])))
+			tmpRow.setRowCell(3, agent.Unsigned32(serverData['send']))
+			tmpRow.setRowCell(4, agent.Unsigned32(serverData['recv']))
+			for u in serverData['users']:
+				tmpUser = userTable.addRow([agent.DisplayString(u['name'])])
+				tmpUser.setRowCell(2, agent.DisplayString(s['name']))
+				tmpUser.setRowCell(3, agent.Unsigned32(int(u['send'])))
+				tmpUser.setRowCell(4, agent.Unsigned32(int(u['recv'])))
+		else:
+			print "{0} is not readable".format(s['logFile'])
 
 	# Since we didn't give simpleCounter, simpleCounter64 and simpleTimeTicks
 	# a real meaning in the SIMPLE-MIB, we can basically do with them whatever
 	# we want. Here, we just increase them, although in different manners.
-	simpleCounter32.update(simpleCounter32.value() + 2)
-	simpleCounter64.update(simpleCounter64.value() + 4294967294)
-	simpleTimeTicks.update(simpleTimeTicks.value() + 1)
+	#simpleCounter32.update(simpleCounter32.value() + 2)
+	#simpleCounter64.update(simpleCounter64.value() + 4294967294)
+	#simpleTimeTicks.update(simpleTimeTicks.value() + 1)
 
 	# With counters, you can also call increment() on them
-	simpleCounter32Context2.increment() # By 1
-	simpleCounter64Context2.increment(5) # By 5
+	#simpleCounter32Context2.increment() # By 1
+	#simpleCounter64Context2.increment(5) # By 5
 
 print "{0}: Terminating.".format(prgname)
 agent.shutdown()
